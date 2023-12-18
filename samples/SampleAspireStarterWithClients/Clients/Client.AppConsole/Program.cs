fd0a081a47a999804286f97e3fe648095aab5aa7
moljac <mcvjetko@holisticware.net>
2023-12-18 21:47:38 +0100

    several console apps for testing PoCs

---
+++ samples/SampleAspireStarterWithClients/Clients/Client.AppConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Net.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+Microsoft.Extensions.Hosting.HostApplicationBuilder builder;
+
+builder = Microsoft.Extensions.Hosting.Host
+                                        .CreateApplicationBuilder(args);
+
+/*
+TODO: 
+    apiservice
+    localhost:5393
+*/
+
+// HttpClient client = new ()                                        
+//                         {
+//                             client.BaseAddress = new("http://apiservice");
+//                         };
+
+
+for (int i=1; ; i++ )
+{
+    Console.WriteLine($"Console");
+    Console.WriteLine($"    Hello, World! {i}");
+    Thread.Sleep(1000);
+}
+
+return;
