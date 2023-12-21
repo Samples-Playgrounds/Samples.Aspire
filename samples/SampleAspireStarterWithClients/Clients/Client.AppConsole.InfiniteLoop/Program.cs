a0ca39ffe23cca36038b0915574d528eb3c7e138
moljac <mcvjetko@holisticware.net>
2023-12-21 20:35:18 +0100

    cleanup

--- samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.InfiniteLoop/Program.cs
+++ samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.InfiniteLoop/Program.cs
@@ -1,28 +1,7 @@
 ﻿
-using System.Net.Http;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-
-Microsoft.Extensions.Hosting.HostApplicationBuilder builder;
-
-builder = Microsoft.Extensions.Hosting.Host
-                                        .CreateApplicationBuilder(args);
-
-/*
-TODO: 
-    apiservice
-    localhost:5393
-*/
-
-// HttpClient client = new ()                                        
-//                         {
-//                             client.BaseAddress = new("http://apiservice");
-//                         };
-
-
 for (int i=1; ; i++ )
 {
-    Console.WriteLine($"Console");
+    Console.WriteLine($"Console launched by Aspire and rolling...");
     Console.WriteLine($"    Hello, World! {i}");
     Thread.Sleep(1000);
 }
