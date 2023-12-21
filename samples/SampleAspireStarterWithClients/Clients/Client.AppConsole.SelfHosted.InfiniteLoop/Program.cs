57ae4cafcb6c337a3a263bfbb79218d878885812
moljac <mcvjetko@holisticware.net>
2023-12-21 20:35:55 +0100

    new project client console app

---
+++ samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.SelfHosted.InfiniteLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Net.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+Microsoft.Extensions.Hosting.HostApplicationBuilder builder;
+
+builder = Microsoft.Extensions.Hosting.Host
+                                        .CreateApplicationBuilder(args);
+
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
+
+/*
+TODO: 
+    apiservice
+    localhost:5393
+*/
+
+// HttpClient client = new ()                                        
+//                         {
+//                             client.BaseAddress = new("http://apiservice");
+//                         };
+
+
+for (int i=1; ; i++ )
+{
+    Console.WriteLine($"Console launched by Aspire and rolling...");
+    Console.WriteLine($"    Hello, World! {i}");
+    
+    Thread.Sleep(1000);
+}
+
+return;
---
+++ samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.With.OpenTelemetry/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
+
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+
+// ...
+
+var serviceName = "Client.AppConsole.With.OpenTelemetry";
+var serviceVersion = "1.0.0";
+
+using TracerProvider tracerProvider = Sdk
+                                        .CreateTracerProviderBuilder()
+                                        .AddSource(serviceName)
+                                        .SetResourceBuilder
+                                                (
+                                                    ResourceBuilder.CreateDefault()
+                                                    .AddService
+                                                            (
+                                                                serviceName: serviceName, 
+                                                                serviceVersion: serviceVersion
+                                                            )
+                                                )
+                                        .AddConsoleExporter()
+                                        .Build();
+
+
+Console.WriteLine("Console.WriteLine - Hello, World! Launched by Aspire");
+Trace.WriteLine("Trace.WriteLine - Hello, World! Launched by Aspire");
