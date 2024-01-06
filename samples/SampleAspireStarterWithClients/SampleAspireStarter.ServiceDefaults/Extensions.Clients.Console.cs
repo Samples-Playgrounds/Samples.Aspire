71513752ff05208b9a358bc000e15f687e450fb2
moljac <mcvjetko@holisticware.net>
2024-01-06 17:48:20 +0100

    Create Extensions.Clients.Console.cs

---
+++ samples/SampleAspireStarterWithClients/SampleAspireStarter.ServiceDefaults/Extensions.Clients.Console.cs
@@ -0,0 +1,15 @@
+//using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static partial class 
+                                        Extensions
+{
+}
