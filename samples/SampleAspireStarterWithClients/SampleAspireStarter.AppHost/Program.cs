27259280193d78423ac08c485ac3bb45201e9f85
moljac <mcvjetko@holisticware.net>
2024-01-10 10:06:03 +0100

    formatting + simple console app with OpenTelemetry sample

--- samples/SampleAspireStarterWithClients/SampleAspireStarter.AppHost/Program.cs
+++ samples/SampleAspireStarterWithClients/SampleAspireStarter.AppHost/Program.cs
@@ -16,41 +16,43 @@ builder
                                     (
                                         "webfrontend"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppConsole>
                                     (
                                         "client_app_console"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppConsole_Configuration>
                                     (
                                         "client_app_console_configuration"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
+
 builder
     .AddProject<Projects.Client_AppConsole_Configuration_NotNot_AppSettings>
                                     (
                                         "client_app_console_configuration_notnot_appsettings"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppConsole_InfiniteLoop>
                                     (
                                         "client_app_console_infinite_loop"
                                     )
-    // localhost:PORT           possible
-    // IP_PRIVATE_NETWORK:PORT  possible??
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
+
+builder
+    .AddProject<Projects.Client_AppConsole_With_OpenTelemetry>
+                                    (
+                                        "client_app_console_with_opentelemetry"
+                                    )
+    .WithReference(apiservice);
+
 builder
     .AddProject<Projects.Client_AppConsole_SelfHosted_InfiniteLoop>
                                     (
@@ -58,40 +60,35 @@ builder
                                     )
     // localhost:PORT           possible
     // IP_PRIVATE_NETWORK:PORT  possible??
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppConsole_SelfHosted_With_Configuration>
                                     (
                                         "client_app_console_selfhosted_with_configuration"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppAvalonia_MVVM>
                                     (
                                         "client_app_avalonia_mvvm"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppAvalonia_XPlat_Browser>
                                     (
                                         "client_app_avalonia_xplat_browser"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 builder
     .AddProject<Projects.Client_AppAvalonia_XPlat_Desktop>
                                     (
                                         "client_app_avalonia_xplat_desktop"
                                     )
-    .WithReference(apiservice)
-    ;
+    .WithReference(apiservice);
 
 
 #if __BRAINSTORMING__
