f64cfee3c4cb14e31c3908caa8263bafdfb2c8e0
moljac <mcvjetko@holisticware.net>
2024-01-10 10:05:31 +0100

    simple console app with OpenTelemetry (tracing and metrics)

--- samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.With.OpenTelemetry/Program.cs
+++ samples/SampleAspireStarterWithClients/Clients/Client.AppConsole.With.OpenTelemetry/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System.Diagnostics;
-// See https://aka.ms/new-console-template for more information
-
+using Microsoft.Extensions.Logging;
 using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
+using System.Diagnostics.Metrics;
 
-// ...
+using
+    ILoggerFactory loggerFactory = LoggerFactory.Create
+                                        (
+                                            builder => 
+                                            {
+                                                builder.AddOpenTelemetry
+                                                            (
+                                                                options => 
+                                                                {
+                                                                   options.AddConsoleExporter();
+                                                                }
+                                                            );
+                                            }
+                                        );
 
+ILogger<Program> logger = loggerFactory.CreateLogger < Program > ();
+logger.LogInformation("Hello from OpenTelemetry");
+
+
+
+Meter MyMeter = new("ConsoleDemo.Metrics", "1.0");
+
+Counter<long> RequestCounter = MyMeter.CreateCounter<long>("RequestCounter");
+
+using MeterProvider meterProvider = Sdk.CreateMeterProviderBuilder()
+                                                .AddMeter("ConsoleDemo.Metrics")
+                                                .AddConsoleExporter()
+                                                .Build();
+
+RequestCounter.Add(1, new KeyValuePair<string, object?>("POST Request", HttpMethod.Post));
+RequestCounter.Add(1, new KeyValuePair<string, object?>("GET Request", HttpMethod.Get));
+RequestCounter.Add(1, new KeyValuePair<string, object?>("GET Request", HttpMethod.Get));
+RequestCounter.Add(1, new KeyValuePair<string, object?>("POST Request", HttpMethod.Post));
+RequestCounter.Add(1, new KeyValuePair<string, object?>("PUT Request", HttpMethod.Put));
+
+
+/*
 string service_name = "Client.AppConsole.With.OpenTelemetry";
 string service_version = "1.0.0";
 
@@ -18,14 +55,36 @@ using TracerProvider tracerProvider = Sdk
                                                     ResourceBuilder.CreateDefault()
                                                     .AddService
                                                             (
-                                                                serviceName: service_name, 
+                                                                serviceName: service_name,
                                                                 serviceVersion: service_version
                                                             )
                                                 )
                                         .AddConsoleExporter()
                                         .Build();
+*/
+ActivitySource MyActivitySource = new("ConsoleDemo.Trace");
+using
+    TracerProvider tracer_provider = Sdk
+                                        .CreateTracerProviderBuilder()
+                                        .AddSource("ConsoleDemo.Trace")
+                                        .AddConsoleExporter()
+                                        .Build();
 
-
+using(var activity = MyActivitySource.StartActivity("ActivityStarted")) 
+{
+    int start_number = 10000;
+    activity?.SetTag("StartNumber", start_number);
+    for (int i = 0; i < start_number; i++) 
+    {
+        DoProcess(i);
+    }
+    activity?.SetStatus(ActivityStatusCode.Ok);
+}
+void DoProcess(int currentNumber) 
+{
+    var doubleValue = currentNumber * 2;
+}    
+    
 Console.WriteLine("Console.WriteLine - Hello, World! Launched by Aspire");
 System.Diagnostics.Trace.WriteLine
                             (
